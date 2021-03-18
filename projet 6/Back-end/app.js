2758da55fe56ca2c07496d3ee42d1835da61d864
Squaloshiro <mehdi.link@hotmail.fr>
2021-03-18 18:29:52 +0100

    back 50%

--- projet 6/Back-end/app.js
+++ projet 6/Back-end/app.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const path = require('path');
+const stuffRoutes = require('./routes/stuff')
+const userRoutes = require('./routes/user');
 
-//const mongoose = require('mongoose');
 
-//const Thing = require('./model/thing');
 
-/*mongoose.connect('mongodb+srv://Squaloshiro:Presea73@cluster0.ax98j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://Squaloshiro:Presea73@cluster0.ax98j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
     .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'));*/
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
 const app = express();
@@ -28,101 +30,8 @@ app.use((req, res, next) => {
 
 
 app.use(bodyParser.json())
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauces', stuffRoutes)
+app.use('/api/auth', userRoutes);
 
-
-
-app.post('/api/sauces', (req, res, next) => {
-    console.log('--------------req.body----------------------');
-    console.log(req.body);
-    console.log('------------------------------------');
-    // delete req.body._id
-    //const thing = new Thing({
-    //     ...req.body
-    // })
-    // console.log('----------------thing--------------------');
-    //console.log(thing);
-    // console.log('------------------------------------');
-    // thing.save()
-    //   .then(() => {
-    //    console.log('-------------res-----------------------');
-    //    console.log("ok");
-    //   console.log('------------------------------------');
-    res.status(201).json({ message: 'Objet enregistré ! ' })
-
-
-    //  })
-    // .catch(error => res.status(400).json({ error }))
-});
-
-app.use('/api/auth/signup', (req, res, next) => {
-    const signup = [
-        {
-            email: 'email',
-            password: 'password',
-        },
-
-    ];
-    res.status(200).json(signup);
-});
-
-app.use('/api/auth/login', (req, res, next) => {
-    const signup = [
-        {
-            email: 'email',
-            password: 'password',
-        },
-
-    ];
-    res.status(200).json(signup);
-});
-
-
-
-app.use('/api/sauces', (req, res, next) => {
-    const sauce = [{
-        _id: 'eroimfgjlfh',
-        name: 'Blair\'s Ultra Death Sauce',
-        manufacturer: 'Blair\'s',
-        description: 'Blair\'s Ultra Death has established itself as a bit of a legend within the hot sauce world.\n' +
-            '\n' +
-            'If there\'s one thing that creator Blair Lazar does well it\'s retaining the flavour in his super-hot sauces. They\'ll melt your face off for sure, but despite the extract they still taste damned fine.\n' +
-            '\n' +
-            'Just to emphasise the seriousness of the heat we\'re dealing with here, all Blair\'s super-hot sauces in the Death range now come in a nifty coffin box with his trademark skull keyring attached to the bottle.',
-        heat: 9,
-        likes: 100,
-        dislikes: 0,
-        imageUrl: 'https://www.chilliworld.com/content/images/thumbs/0000827_blairs-ultra-death-sauce-in-a-coffin_550.jpeg',
-        mainPepper: 'Carolina Reaper',
-        usersLiked: [],
-        usersDisliked: []
-    }]
-    res.status(200).json(sauce);
-    /* Thing.find()
-         .then(things => res.status(200).json(things))
-         .catch(error => res.status(400).json({ error }))*/
-});
-
-app.use('/api/sauces/:id', (req, res, next) => {
-    const sauce = [{
-        _id: 'eroimfgjlfh',
-        name: 'Blair\'s Ultra Death Sauce',
-        manufacturer: 'Blair\'s',
-        description: 'Blair\'s Ultra Death has established itself as a bit of a legend within the hot sauce world.\n' +
-            '\n' +
-            'If there\'s one thing that creator Blair Lazar does well it\'s retaining the flavour in his super-hot sauces. They\'ll melt your face off for sure, but despite the extract they still taste damned fine.\n' +
-            '\n' +
-            'Just to emphasise the seriousness of the heat we\'re dealing with here, all Blair\'s super-hot sauces in the Death range now come in a nifty coffin box with his trademark skull keyring attached to the bottle.',
-        heat: 9,
-        likes: 100,
-        dislikes: 0,
-        imageUrl: 'https://www.chilliworld.com/content/images/thumbs/0000827_blairs-ultra-death-sauce-in-a-coffin_550.jpeg',
-        mainPepper: 'Carolina Reaper',
-        usersLiked: [],
-        usersDisliked: []
-    }]
-    res.status(200).json(sauce);
-    /* Thing.find()
-         .then(things => res.status(200).json(things))
-         .catch(error => res.status(400).json({ error }))*/
-});
 module.exports = app;
